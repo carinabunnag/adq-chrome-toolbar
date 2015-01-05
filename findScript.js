89f9a864068b7bc189c676ab5afc852a826145cb
Carina Bunnag <cb2508@nyu.edu>
2015-01-05 16:05:14 -0500

    major updates

--- findScript.js
+++ findScript.js
@@ -14,33 +14,6 @@ function DOMtoString(document_root) {
   return html;
 };
 
-function DOMtoString(document_root) {
-  var html = '',
-  node = document_root.firstChild;
-  while (node) {
-    switch (node.nodeType) {
-      case Node.ELEMENT_NODE:
-        html += node.outerHTML;
-        break;
-        case Node.TEXT_NODE:
-          html += node.nodeValue;
-          break;
-          case Node.CDATA_SECTION_NODE:
-            html += '<![CDATA[' + node.nodeValue + ']]>';
-            break;
-            case Node.COMMENT_NODE:
-              html += '<!--' + node.nodeValue + '-->';
-              break;
-              case Node.DOCUMENT_TYPE_NODE:
-                // (X)HTML documents are identified by public identifiers
-                html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-                break;
-              }
-              node = node.nextSibling;
-            }
-            return html;
-          }
-
 // find publisher script in head
 function hasScript() {
   var script = DOMtoString(document.head).match(/cdn+\.+adjs+\.+net\/publisher+\.+ad+\.+min+\.+js/);
@@ -49,6 +22,38 @@ function hasScript() {
   else
     return 0;
 }
+
+/* find ads on webpage */
+
+//FIRST TEST
+//check for div name "advman-ad-" and return array of names
+function div_name() {
+  var count = 1,
+    name = [];
+  while (document.getElementById("advman-ad-" + count)) {
+    name[name.length] = document.getElementById("advman-ad-" + count);
+    count++;
+  }
+  return name;
+}
+
+
+//SECOND TEST
+//check for google script
+function google_script() {
+  var script = document.getElementByTagName("script");
+
+}
+
+//find ad frames and returns an array of iframes
+function get_iframes() {
+  var iframes = document.getElementsByTagName("iframe");
+  return iframes;
+}
+
+// function findAdFrame() {
+//   var adframes =
+// }
 
 //find publisher append script
 function findAppend() {
@@ -61,28 +66,23 @@ function findAppend() {
     return 0;
 }
 
-//returns iframe elements
-function getAdFrames() {
-  var iframes = document.getElementsByTagName("iframe");
-  return iframes;
+//returns array of all ads on the page
+// function ads() {
+//   var ads = [];
+//   var divs = div_name();
+//   if (divs)
+//      ads = ads.concat(divs);
+//   if
+//
+//   return ads;
+// }
+
+//returns number of all ads on the page
+function adCount() {
+  var ads = ads();
+  return ads.length;
 }
 
-/* default ad sizes:
-[“120x60”,
-“120x600”,
-“160x600”,
-“180x150”,
-“300x1050”,
-“300x250”,
-“300x600”,
-“468x60”,
-“550x480”,
-“728x90”,
-“88x31”,
-“970x250”,
-“970x66”,
-“970x90”]
-*/
 
 //send message to background.js
 chrome.runtime.sendMessage({
